35b1b5ef9e7a5fb9dfd3b1f2a8ff7b4f4cfc04f4
Carlos H. Marian <carlos.marian@amcom.com.br>
2019-05-15 19:00:06 -0300

    Basic using Controllers, create methods

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,9 +1,16 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 
 namespace NetCore2.Controllers
 {
     public class AdminController : Controller
     {
-        
+        public IActionResult Index(){
+            return Content("Olá Mundo");
+        }
+
+        public IActionResult SchoolOfNet(){
+            return Content("Novo teste");
+        }
     }
 }
