b2aa29a54e831487575f9b581ed0e98bb90c0b2d
Carlos H. Marian <carlos.marian@amcom.com.br>
2019-05-15 19:29:02 -0300

    Basic using Rota, configuration types and optional parameters

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -3,12 +3,32 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace NetCore2.Controllers
 {
+    //Alterando a rota padrão do controller
+    [Route("painel/admin")]
     public class AdminController : Controller
     {
-        public IActionResult Index(){
-            return Content("Olá Mundo");
+        
+        //Se mudar a rota padrão do controller, deve mapear cada metodo com verbo HTTP
+        //De deixar vazio ele usa o nome do metodo. Pode ter mais de um mapeamento para o mesmo metodo.
+        //[HttpGet("")]
+        //metodo com parametro
+        [HttpGet("principal/{nome}")]
+        public IActionResult Index(string nome){
+            return Content("Olá "+ nome);
         }
 
+        //Tipando o parametro
+        [HttpGet("numero/{num:int}")]
+        public IActionResult Numero(int num){
+            return Content("O nro é: "+ num);
+        }
+
+        [HttpGet("opcional/{nome?}")]
+        public IActionResult Opcional(string nome){
+            return Content("Olá "+ nome);
+        }
+
+        [HttpGet("son")]
         public IActionResult SchoolOfNet(){
             return Content("Novo teste");
         }
